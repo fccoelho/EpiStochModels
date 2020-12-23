20d8eeea37cd31334351e058d1a1b700aeea0578
Flávio Codeço Coelho <fccoelho@gmail.com>
2020-12-23 11:03:01 -0300

    added readthedocs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Flavio Codeço Coelho'
 author = 'Flavio Codeço Coelho'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
